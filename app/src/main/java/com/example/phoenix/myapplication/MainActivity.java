8c241453e29f55c54b1ea74566e21408a2df880d
JACK_GUANG <o365flysky@icloud.com>
2016-07-11 09:48:51 +0800

    Test，fitst commit project to github

---
+++ app/src/main/java/com/example/phoenix/myapplication/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.phoenix.myapplication;
+
+import android.content.res.AssetManager;
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView text = new TextView(this);
+        String fontPath = "fonts/";
+        AssetManager manager = getAssets();
+        Typeface tf = Typeface.createFromAsset(manager,fontPath);
+        text.setTypeface(tf);
+    }
+}
